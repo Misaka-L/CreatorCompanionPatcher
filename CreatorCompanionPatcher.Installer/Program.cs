8a895dfd35511c468af1356d08d1eeb5b8889299
Misaka-L <lipww1234@foxmail.com>
2024-01-31 23:15:26 +0800

    feature(installer): support vcc install at non-default location

--- CreatorCompanionPatcher.Installer/Program.cs
+++ CreatorCompanionPatcher.Installer/Program.cs
@@ -2,6 +2,7 @@
 using System.Net.Http.Headers;
 using System.Reflection;
 using CreatorCompanionPatcher.Core;
+using Microsoft.Win32;
 using Serilog;
 using Serilog.Sinks.SystemConsole.Themes;
 
@@ -31,16 +32,24 @@ Log.Information("VRChat Creator Companion Installer v{Version}", Assembly.GetExe
 var creatorCompanionPath = LookupCreatorCompanion();
 if (creatorCompanionPath is null)
 {
-    Log.Information("VRChat Creator Companion not found. Please install it first. Press any key to exit");
+    Log.Information("VRChat Creator Companion Path not found. Please install it first. Press any key to exit");
     Console.ReadLine();
     return;
 }
 
-Log.Information("VRChat Creator Companion found at: {VccPath}", creatorCompanionPath);
-
-if (LookupCreatorCompanionPatcher() is not { } patcherPath)
+var creatorCompanionExecutablePath = GetCreatorCompanionExecutablePath(creatorCompanionPath);
+if (creatorCompanionExecutablePath is null)
 {
-    var installedPatcherPath = await InstallPatcherAsync();
+    Log.Information("VRChat Creator Companion Executable not found. Please install it first. Press any key to exit");
+    Console.ReadLine();
+    return;
+}
+
+Log.Information("VRChat Creator Companion found at: {VccPath}", creatorCompanionExecutablePath);
+
+if (LookupCreatorCompanionPatcher(creatorCompanionPath) is not { } patcherPath)
+{
+    var installedPatcherPath = await InstallPatcherAsync(creatorCompanionPath);
     StartApp(installedPatcherPath);
     return;
 }
@@ -57,7 +66,7 @@ if (await UpdateChecker.CheckIsNewerReleaseAvailableAsync(patcherVersion) is { }
 {
     Log.Information("Newer release available: {Version}", release.TagName);
     Log.Information("Installing New Version...");
-    await InstallPatcherByUrlAsync(newVersionDownloadUrl, release.TagName);
+    await InstallPatcherByUrlAsync(newVersionDownloadUrl, release.TagName, creatorCompanionPath);
     Log.Information("Patcher Installed");
 }
 else
@@ -69,51 +78,78 @@ StartApp(patcherPath);
 
 return;
 
-static string? LookupCreatorCompanion()
-{
-    var localAppData = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
-    var creatorCompanionPath = Path.Combine(localAppData, "Programs/VRChat Creator Companion/CreatorCompanion.exe");
+#region Usefull or useless methods
 
-    return File.Exists(creatorCompanionPath) ? creatorCompanionPath : null;
+#region Lookup CreatorCompanion & Patcher
+
+static string? GetCreatorCompanionExecutablePath(string installLocation)
+{
+    return Path.Combine(installLocation, "CreatorCompanion.exe");
 }
 
-static string? LookupCreatorCompanionPatcher()
+static string? LookupCreatorCompanion()
 {
+    if (LookupCreatorCompanionInRegistry() is { } installLocation)
+        return installLocation;
+
     var localAppData = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
-    var patcherPath = Path.Combine(localAppData, "Programs/VRChat Creator Companion/CreatorCompanionPatcher.exe");
+    var creatorCompanionPath = Path.Combine(localAppData, "Programs/VRChat Creator Companion");
+
+    return Directory.Exists(creatorCompanionPath) ? creatorCompanionPath : null;
+}
+
+static string? LookupCreatorCompanionInRegistry()
+{
+    const string installedAppsRegistryKey = @"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall";
+
+    if (LookupCreatorCompanionPathInRegistry(Registry.LocalMachine, installedAppsRegistryKey) is
+        { } globalInstallLocation)
+        return globalInstallLocation;
+
+    if (LookupCreatorCompanionPathInRegistry(Registry.CurrentUser, installedAppsRegistryKey) is { } userInstallLocation)
+        return userInstallLocation;
+
+    return null;
+}
+
+static string? LookupCreatorCompanionPathInRegistry(RegistryKey registryKey, string installedAppsRegistryKey)
+{
+    using var key = registryKey.OpenSubKey(installedAppsRegistryKey);
+
+    if (key is null)
+        throw new InvalidOperationException("Unable to find registry key: " + installedAppsRegistryKey +
+                                            ", maybe there is something wrong in your system?");
+
+    foreach (var appId in key.GetSubKeyNames())
+    {
+        using var appKey = key.OpenSubKey(appId);
+
+        if (appKey?.GetValue("DisplayName") is not string displayName) continue;
+
+        if (!displayName.Contains("VRChat Creator Companion")) continue;
+        if (appKey.GetValue("InstallLocation") is not string installLocation) continue;
+
+        return installLocation;
+    }
+
+    return null;
+}
+
+
+static string? LookupCreatorCompanionPatcher(string creatorCompanionInstallLocation)
+{
+    var patcherPath = Path.Combine(creatorCompanionInstallLocation,
+        "CreatorCompanionPatcher.exe");
 
     return File.Exists(patcherPath) ? patcherPath : null;
 }
 
-static string GetPatcherVersion(string path)
+#endregion
+
+#region Install Patcher
+
+static async Task<string> InstallPatcherByUrlAsync(string patcherFileUrl, string version, string vccPath)
 {
-    var vccPath = Path.GetDirectoryName(path);
-    if (vccPath is null)
-        throw new InvalidOperationException("Could not get patcher directory path.");
-
-    var patcherVersionPath = Path.Combine(vccPath, patcherVersionDataFile);
-
-    if (!File.Exists(patcherVersionPath))
-        throw new FileNotFoundException("Could not find patcher version file.", patcherVersionPath);
-
-    return File.ReadAllText(patcherVersionPath);
-}
-
-static void StartApp(string patcherPath)
-{
-    Log.Information("Starting VRChat CreatorCompanion...");
-
-    Process.Start(new ProcessStartInfo(patcherPath)
-    {
-        WorkingDirectory = Path.GetDirectoryName(patcherPath),
-        UseShellExecute = true
-    });
-}
-
-static async Task<string> InstallPatcherByUrlAsync(string patcherFileUrl, string version)
-{
-    var localAppData = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
-    var vccPath = Path.Combine(localAppData, "Programs/VRChat Creator Companion");
     var patcherPath = Path.Combine(vccPath, "CreatorCompanionPatcher.exe");
     var versionDataPath = Path.Combine(vccPath, patcherVersionDataFile);
 
@@ -143,10 +179,39 @@ static async Task<string> InstallPatcherByUrlAsync(string patcherFileUrl, string
     return patcherPath;
 }
 
-static async Task<string> InstallPatcherAsync()
+static async Task<string> InstallPatcherAsync(string vccPath)
 {
     Log.Information("Fetching latest release from GitHub...");
     var release = await UpdateChecker.GetLatestReleaseAsync();
 
-    return await InstallPatcherByUrlAsync(newVersionDownloadUrl, release.TagName);
+    return await InstallPatcherByUrlAsync(newVersionDownloadUrl, release.TagName, vccPath);
 }
+
+#endregion
+
+static string GetPatcherVersion(string path)
+{
+    var vccPath = Path.GetDirectoryName(path);
+    if (vccPath is null)
+        throw new InvalidOperationException("Could not get patcher directory path.");
+
+    var patcherVersionPath = Path.Combine(vccPath, patcherVersionDataFile);
+
+    if (!File.Exists(patcherVersionPath))
+        throw new FileNotFoundException("Could not find patcher version file.", patcherVersionPath);
+
+    return File.ReadAllText(patcherVersionPath);
+}
+
+static void StartApp(string patcherPath)
+{
+    Log.Information("Starting VRChat CreatorCompanion...");
+
+    Process.Start(new ProcessStartInfo(patcherPath)
+    {
+        WorkingDirectory = Path.GetDirectoryName(patcherPath),
+        UseShellExecute = true
+    });
+}
+
+#endregion
