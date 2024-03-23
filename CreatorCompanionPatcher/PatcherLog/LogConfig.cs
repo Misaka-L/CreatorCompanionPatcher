8e2ba5ae946129e79982c8360dc84a168ab90e5c
Misaka-L <lipww1234@foxmail.com>
2024-03-23 14:05:08 +0800

    fix: logs write to somewhere it shouldn't be

--- CreatorCompanionPatcher/PatcherLog/LogConfig.cs
+++ CreatorCompanionPatcher/PatcherLog/LogConfig.cs
@@ -2,5 +2,7 @@
 
 public static class LogConfig
 {
-    public static readonly string LogPath = Path.GetFullPath($"patcher-logs/patcher-{DateTimeOffset.Now:yyyy-MM-dd-HH-mm-ss}-{Guid.NewGuid():D}.log");
+    public static readonly string LogPath =
+        Path.Join(AppDomain.CurrentDomain.BaseDirectory,
+            $"patcher-logs/patcher-{DateTimeOffset.Now:yyyy-MM-dd-HH-mm-ss}-{Guid.NewGuid():D}.log");
 }
